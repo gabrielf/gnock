dd904e10dcdd54a0b28bf43aeb654acd1a6bdc13
Gabriel Falkenberg <gabriel@soundtrackyourbrand.com>
2018-02-14 22:59:02 +0100

    Add some godoc comments for main entry points.

--- gnock.go
+++ gnock.go
@@ -1,3 +1,4 @@
+// Package gnock is used to mock HTTP responses in tests.
 package gnock
 
 import (
@@ -5,10 +6,17 @@ import (
 	"regexp"
 )
 
+// Gnock is one of the entry points for setting up mock responses. It takes
+// the host that Gnock will intercept and returns a Scope which is used to
+// setup the response(s) to send.
 func Gnock(host string) *Scope {
 	return NewScope(nil, host)
 }
 
+// GnockRegexp is another entry points for setting up mock responses. The
+// difference from the Gnock(…) function is that the host will be compiled
+// as a normal go regexp to enable matching a broader set of hosts. If you
+// want to match any host simply use this method with the parameter ".*"
 func GnockRegexp(host string) *Scope {
 	return NewRegexpScope(nil, regexp.MustCompile(host))
 }
